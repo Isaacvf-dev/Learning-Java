067a7275db4c23635baa6586ac79bf385f56a65c
Isaac Francelino <isaacvf.dev@gmail.com>
2024-07-15 16:26:52 -0300

    Default

--- Main.java
+++ Main.java
@@ -4,13 +4,7 @@ package curso_java;
 public class Main {
 
 	public static void main(String[] args) {
-		int a, b;
-		double resultado;
-		a = 5;
-		b = 2;
 		
-		resultado = (double) a / b;
-		System.out.println(resultado);
 		
 	}
 
