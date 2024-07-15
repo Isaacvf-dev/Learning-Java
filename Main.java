5fa2af1c1147e62d585f0abc62d45e99f0fc5c2f
Isaac Francelino <isaacvf.dev@gmail.com>
2024-07-15 16:19:34 -0300

    Reading text until next line

--- Main.java
+++ Main.java
@@ -7,18 +7,16 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		String x;
-		int y;
-		double z;
+		String s1, s2, s3;
 		
-		x = sc.next();
-		y = sc.nextInt();
-		z = sc.nextDouble();
+		s1 = sc.nextLine();
+		s2 = sc.nextLine();
+		s3 = sc.nextLine();
 		
 		System.out.println("Dados digitados: ");
-		System.out.println(x);
-		System.out.println(y);
-		System.out.printf("%.2f", z);	
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);	
 		
 		sc.close();
 	}
